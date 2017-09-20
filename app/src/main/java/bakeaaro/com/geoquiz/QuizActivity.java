d6a2e4383c71e592b5ce61acf2d7702f70ff8904
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-09-20 14:55:33 -0700

    moved toast message to top per challenge request

--- app/src/main/java/bakeaaro/com/geoquiz/QuizActivity.java
+++ app/src/main/java/bakeaaro/com/geoquiz/QuizActivity.java
@@ -3,9 +3,13 @@ package bakeaaro.com.geoquiz;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.Gravity;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -22,15 +26,41 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // 'this' would refer to the anonymous class
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                //toast.setGravity(Gravity.TOP |Gravity.LEFT, 0, 0);
+
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.custom_toast,
+                        (ViewGroup) findViewById(R.id.toast_LinearLayout));
+
+                TextView text = (TextView) layout.findViewById(R.id.toast_TV);
+                text.setText("Correct!");
+
+                Toast toast = new Toast(QuizActivity.this);
+                toast.setGravity(Gravity.TOP, 0, 150);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setView(layout);
+                toast.show();
+
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.custom_toast,
+                        (ViewGroup) findViewById(R.id.toast_LinearLayout));
 
+                TextView text = (TextView) layout.findViewById(R.id.toast_TV);
+                text.setText("Incorrect!");
+
+                Toast toast = new Toast(QuizActivity.this);
+                toast.setGravity(Gravity.TOP, 0, 150);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setView(layout);
+                toast.show();
             }
         });
 
